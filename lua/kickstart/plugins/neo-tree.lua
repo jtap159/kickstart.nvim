16d66845b98ec6cbbf5b120a8b07481e54d70914
Jeremy Tapia <jtapia@clarityinnovates.com>
2026-05-07 23:46:28 -0400

    adding markdown viewer

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -16,6 +16,7 @@ vim.pack.add(plugins)
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
+  close_if_last_window = true,
   filesystem = {
     window = {
       mappings = {
